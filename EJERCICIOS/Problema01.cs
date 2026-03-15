f7407c426a66c4df050111582a0d57e28200f972
cantudedgar-hue <cantudedgar@gmail.com>
2026-03-15 13:00:44 -0600

    se a realizado con exito el ejercicio1

--- EJERCICIOS/Problema01.cs
+++ EJERCICIOS/Problema01.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PROJECTEULER.EJERCICIOS
+﻿namespace PROJECTEULER.EJERCICIOS
 {
     internal class Problema01
     {
+        public static int Resolver(int n)
+        {
+            List<int> lista = new List<int>();
+            int resultado = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                lista.Add(i);
+            }
+
+            List<int> multiplos = ObtenerMultiplosDeTresYCinco(lista);
+            foreach (int i in multiplos)
+            {
+                resultado += i;
+            }
+            return resultado;
+        }
+
+        static List<int> ObtenerMultiplosDeTresYCinco(List<int> lista_f)
+        {
+            List<int> resultado = new List<int>();
+            foreach (int i in lista_f)
+            {
+
+                if (i % 3 == 0)
+                {
+                    resultado.Add(i);
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    resultado.Add(i);
+                    continue;
+                }
+            }
+            return resultado;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿using PROJECTEULER.EJERCICIOS;
 
+Console.WriteLine(Problema01.Resolver(1000));
 Console.WriteLine(Problema06.Resolver(100));
