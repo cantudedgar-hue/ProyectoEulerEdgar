793dd81ce3719a85e6ca54f39f142f8fc6a75ab6
cantudedgar-hue <cantudedgar@gmail.com>
2026-03-16 09:34:01 -0600

    Se a finalizado con el ejercicio 2

---
+++ EJERCICIOS/Problema02.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECTEULER.EJERCICIOS
+{
+    internal class Problema02
+    {
+        public static long Resolver(int n)
+        {
+            long resultado = SucecionFibonacci(n);
+            
+            return resultado;
+        }
+
+        static long SucecionFibonacci(long num_f)
+        {
+            List<long> lista_fibonacci = [2];
+            long resultado = 0;
+            long f0 = 1;
+            long f1 = 2;
+            long f2 = 0;
+
+            for (long i = 0; i < num_f + 1; i++)
+            {
+                f2 = f1 + f0;
+                if(!(f2 > 4000000))
+                {
+                    if (f2 % 2 == 0)
+                    {
+                        
+                        lista_fibonacci.Add(f2);
+                    }
+                    
+                    f0 = f1;
+                    f1 = f2;         
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            resultado = lista_fibonacci.Sum();
+
+            return resultado;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using PROJECTEULER.EJERCICIOS;
 
-Console.WriteLine(Problema01.Resolver(1000));
-Console.WriteLine(Problema06.Resolver(100));
+//Console.WriteLine(Problema01.Resolver(1000));
+Console.WriteLine(Problema02.Resolver(4000000));
+//Console.WriteLine(Problema06.Resolver(100));
