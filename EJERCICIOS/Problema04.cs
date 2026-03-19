ba6d9175035478a868d3bf478e964d56753abe36
cantudedgar-hue <cantudedgar@gmail.com>
2026-03-18 20:41:07 -0600

    Se a realizado con exito el problema 4

---
+++ EJERCICIOS/Problema04.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECTEULER.EJERCICIOS
+{
+    internal class Problema04
+    {
+        public static int Resolver()
+        {
+            int resultado = 0;
+            for (int i = 100; i < 1000; i++)
+            {
+                for (int j = 100; j < 1000; j++)
+                {
+                    int producto = i * j;
+                    if (producto.ToString() == new string(producto.ToString().Reverse().ToArray()))
+                    {
+                        if (resultado < producto)
+                        {
+                            resultado = producto;
+                        }    
+                    }
+                }
+            }
+            return resultado;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@
 
 //Console.WriteLine(Problema01.Resolver(1000));
 //Console.WriteLine(Problema02.Resolver(4000000));
-Console.WriteLine(Problema03.Resolver(600851475143));
+//Console.WriteLine(Problema03.Resolver(600851475143));
+Console.WriteLine(Problema04.Resolver());
 //Console.WriteLine(Problema06.Resolver(100));
