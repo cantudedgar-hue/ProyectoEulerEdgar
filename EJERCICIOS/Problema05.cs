3beea9435cbb072528e00a51de4967c07631e351
cantudedgar-hue <cantudedgar@gmail.com>
2026-03-19 19:51:12 -0600

    Se a logrado terminar con exito el problema 5

---
+++ EJERCICIOS/Problema05.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECTEULER.EJERCICIOS
+{
+    internal class Problema05
+    {
+        public static long Resolver()
+        {
+            long limite_inf = 1;
+            long limite_sup = 100;
+
+            while (true)
+            {
+                for (long i = limite_inf; i <= limite_sup; i++)
+                {
+                    bool esValido = true;
+
+                    for (long n = 1; n <= 21; n++)
+                    {
+                        if (i % n != 0)
+                        {
+                            esValido = false;
+                            break;
+                        }
+                    }
+
+                    if (esValido)
+                    {
+                        return i;
+                    }
+                }
+                limite_inf = limite_sup + 1;
+                limite_sup *= 2;
+            }
+        }
+    }
+}
