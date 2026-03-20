86a17ea86480ed5103cc517162b89c3c91391cb0
cantudedgar-hue <cantudedgar@gmail.com>
2026-03-19 20:33:30 -0600

    Se a completado con exito el ejercicio 7

---
+++ EJERCICIOS/Problema07.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECTEULER.EJERCICIOS
+{
+    internal class Problema07
+    {
+        public static long Resolver()
+        {
+            int objetivo = 10001;
+            int contadorPrimosFound = 0;
+            long numeroActual = 1; 
+
+            while (contadorPrimosFound < objetivo)
+            {
+                numeroActual++;
+
+                if (NumesrosPrimos(numeroActual))
+                {
+                    contadorPrimosFound++;
+                }
+            }
+            return numeroActual; 
+        }
+
+        static bool NumesrosPrimos(long num_f)
+        {
+            if (num_f < 2) return false;
+            if (num_f == 2) return true;
+            if (num_f % 2 == 0) return false;
+
+            int limite = (int)Math.Truncate(Math.Sqrt(num_f));
+
+            for (int i = 3; i <= limite; i += 2)
+            {
+                if (num_f % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@
 //Console.WriteLine(Problema02.Resolver(4000000));
 //Console.WriteLine(Problema03.Resolver(600851475143));
 //Console.WriteLine(Problema04.Resolver());
-Console.WriteLine(Problema05.Resolver());
+//Console.WriteLine(Problema05.Resolver());
 //Console.WriteLine(Problema06.Resolver(100));
+Console.WriteLine(Problema07.Resolver());
